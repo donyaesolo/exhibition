3f6bf3765d015b8c9e09dfaab0d61fcb296b57ee
donyaesolo <35720283+donyaesolo@users.noreply.github.com>
2018-03-27 13:23:45 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+function openProject(evt, projectName) {
+    var i, content, tablink;
+    content = document.getElementsByClassName("content");
+    for (i = 0; i < content.length; i++) {
+        content[i].style.display = "none";
+    }
+    tablink = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablink.length; i++) {
+        tablink[i].className = tablink[i].className.replace(" active", "");
+    }
+    document.getElementById(projectName).style.display = "block";
+    evt.currentTarget.className += " active";
+}  
+
+ document.getElementById("defultOpen").click();
